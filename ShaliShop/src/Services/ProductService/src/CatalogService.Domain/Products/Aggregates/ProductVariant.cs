1f9f18754780a8d5d65dc96a0c56abcb9412f653
dani <d.jajimi@outlook.com>
2026-06-12 23:26:50 +0330

    chor: minor changes

--- ShaliShop/src/Services/ProductService/src/CatalogService.Domain/Products/Aggregates/ProductVariant.cs
+++ ShaliShop/src/Services/ProductService/src/CatalogService.Domain/Products/Aggregates/ProductVariant.cs
@@ -1,4 +1,3 @@
- 
 using CatalogService.Domain.Products.Exceptions;
 
 namespace CatalogService.Domain.Products.Aggregates;
@@ -6,17 +5,18 @@ namespace CatalogService.Domain.Products.Aggregates;
 public class ProductVariant
 {
     public string Sku { get; private set; } = null!;
+
     // EF Core maps this
-    private Dictionary<string, string> _options = new();
+    private readonly Dictionary<string, string> _options = new();
     public IReadOnlyDictionary<string, string> Options => _options;
-     
+
     public Money? PriceOverride { get; private set; }
 
     private ProductVariant()
     {
     }
 
-    public ProductVariant(string sku, Dictionary<string, string> options, Money? priceOverride = null)
+    public ProductVariant(string sku, Dictionary<string, string> options, Money? priceOverride = null) : this()
     {
         if (string.IsNullOrWhiteSpace(sku))
             throw new ArgumentNullException(nameof(sku));
